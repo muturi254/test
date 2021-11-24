eb291b49ce4af0fdd6b6cbc262ce61be8a39c1ee
muturi254 <petermuturi74@gmail.com>
2021-11-24 10:27:07 +0300

    configure settings for workflow on github

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class UserTestCase(TestCase):
+
+    def test_user(self):
+        username = 'john'
+        password = 'hello'
+        u = User(username=username)
+        u.set_password(password)
+        u.save()
+        self.assertEqual(username, u.username)
+        self.assertTrue(u.check_password(password))
